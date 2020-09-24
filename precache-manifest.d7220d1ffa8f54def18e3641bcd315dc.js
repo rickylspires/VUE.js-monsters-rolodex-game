6d952ef0a9814a580d81b54e9ea1aff39e3afc23
rickylspires <rickyspires@gmail.com>
2020-09-24 12:37:56 +0100

    Updates

---
+++ precache-manifest.d7220d1ffa8f54def18e3641bcd315dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5ffb8bf24cd6b6ee9c85b254c195db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4fd853b5abebb6846400",
+    "url": "/monsters-rolodex/static/css/main.48816bd5.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd853b5abebb6846400",
+    "url": "/monsters-rolodex/static/js/main.6883af78.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
